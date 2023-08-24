932536776cf3994f1764d25bf16073443bad3bfe
Tomasz Zawadzki <36971391+tozawadzki@users.noreply.github.com>
2023-08-24 18:22:01 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Tomasz Zawadzki",
+  title: "Software Engineer",
+  email: "tomasz.zawadzki@studentambassadors.com",
+  gitHub: "https://github.com/tozawadzki",
+  instagram: "https://www.instagram.com/tvmeq/",
+  linkedIn: "https://www.linkedin.com/in/tomasz-zawadzki-eka/",
 };
 
 const primaryColor = "#4E567E";

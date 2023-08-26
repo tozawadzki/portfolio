6da28d261eabf73ab9f28b5bf1a9bafb0d9b71d1
Tomasz Zawadzki <36971391+tozawadzki@users.noreply.github.com>
2023-08-26 10:35:07 +0000

    changed about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm Computer Science student and the chairman of a scientific organisation EKA.NET.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C#",
+  ".NET",
+  "React.js",
+  "React Native",
+  "SQL",
+  "Project Management",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about programming, project management processes, organizing events, and self-improvement.";
 
 const About = () => {
   return (
